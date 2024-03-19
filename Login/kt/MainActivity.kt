160cec0046ee02e14677e2c29630b0bd292ec7cb
Gabriel Felipe Jess Meira <124531978+gabrielfjmeira@users.noreply.github.com>
2024-03-19 11:53:37 -0300

    Atividade de Login
    .XML e  .KT da atividade

---
+++ Login/kt/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.example.login
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+
+class MainActivity : AppCompatActivity() {
+    var usrLogin = "user"
+    var pwdLogin = "1234"
+
+    val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        if (it.resultCode == RESULT_OK){
+            var resultUser = it.data?.getStringExtra("usernameEdt")
+            if (resultUser != null) {
+                usrLogin = resultUser
+            }
+
+            var resultPwd = it.data?.getStringExtra("passwordEdt")
+            if (resultPwd != null) {
+                pwdLogin = resultPwd
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var btnLogin = findViewById<Button>(R.id.btnLogin)
+        btnLogin.isEnabled = false
+
+        var usernameInput = findViewById<TextView>(R.id.inputUser)
+        var passwordInput = findViewById<TextView>(R.id.inputPassword)
+
+        usernameInput.addTextChangedListener(textWatcher)
+        passwordInput.addTextChangedListener(textWatcher)
+
+        btnLogin.setOnClickListener {
+            if (usernameInput.text.toString() == usrLogin && passwordInput.text.toString() == pwdLogin){
+                val intent = Intent(this, EditData::class.java)
+                intent.putExtra("username", "${usernameInput.text.toString()}")
+                intent.putExtra("password", "${passwordInput.text.toString()}")
+                resultLauncher.launch(intent)
+            }else{
+                showErrorDialog()
+            }
+        }
+
+    }
+
+    private val textWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+            var usernameInput = findViewById<TextView>(R.id.inputUser).text.toString().trim()
+            var passwordInput = findViewById<TextView>(R.id.inputPassword).text.toString().trim()
+            var btnLogin = findViewById<Button>(R.id.btnLogin)
+
+            btnLogin.isEnabled = usernameInput.isNotEmpty() && passwordInput.isNotEmpty()
+        }
+
+    }
+
+    private fun showErrorDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Erro de login")
+        builder.setMessage("Usuário ou senha inválidos.")
+        builder.setPositiveButton("OK") { dialog, _ ->
+            dialog.dismiss()
+        }
+        val dialog = builder.create()
+        dialog.show()
+    }
+}
